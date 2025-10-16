3651a2813cf7797242d3e48b3bc59284c5eba554
Gabriel Scocozza <5552115+gabrigbxp@users.noreply.github.com>
2025-10-16 12:50:50 -0300

    Add TypeScript type definitions

---
+++ allrules.d.ts
@@ -0,0 +1,4 @@
+import type { Linter } from 'eslint';
+
+declare const allRules: Linter.Config[];
+export default allRules;
---
+++ index.d.ts
@@ -0,0 +1,19 @@
+import type { Linter } from 'eslint';
+
+declare const config: Linter.Config[];
+export default config;
+
+export declare const bestPractices: Linter.Config;
+export declare const errors: Linter.Config;
+export declare const node: Linter.Config;
+export declare const style: Linter.Config;
+export declare const variables: Linter.Config;
+export declare const es6: Linter.Config;
+export declare const imports: Linter.Config;
+export declare const strict: Linter.Config;
+export declare const react: Linter.Config;
+export declare const reactA11y: Linter.Config;
+export declare const reactHooks: Linter.Config;
+export declare const typescriptCore: Linter.Config;
+export declare const typescriptVariables: Linter.Config;
+export declare const typescriptBestPractices: Linter.Config;
