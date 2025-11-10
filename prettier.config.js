de673d825aca1deee840606f55d4863945217c58
Gabriel Scocozza <5552115+gabrigbxp@users.noreply.github.com>
2025-11-10 18:44:42 -0300

    fix prettier rule

--- prettier.config.js
+++ prettier.config.js
@@ -5,7 +5,7 @@ export default {
     bracketSpacing: true,
     arrowParens: 'always',
     printWidth: 120,
-    tabWidth: 4,
+    tabWidth: 2,
     useTabs: false,
     endOfLine: 'lf',
     quoteProps: 'as-needed',
